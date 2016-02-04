8aa4942fdc7343774172c08938a15c55ab3d3a14
ernvalentino88 <ern.valentino@gmail.com>
2016-02-04 17:09:04 +0100

    client connection

--- Client/Client/MainWindow.xaml.cs
+++ Client/Client/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MahApps.Metro.Controls;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Client
 {
@@ -25,6 +28,75 @@ namespace Client
         {
             InitializeComponent();
             
+        }
+
+        private void Connect_button_Click(object sender, RoutedEventArgs e)
+        {
+            TcpClient tcpclnt = new TcpClient();
+            try
+            {
+                Int32 port = Int32.Parse(this.Text_port.Text);
+                tcpclnt.Connect(this.Text_ip.Text, port);
+                Stream stm = tcpclnt.GetStream();
+                String str = "ar.pdf";
+
+                Console.WriteLine("Sending file infos...");
+
+                Console.WriteLine("Transmitting file size.....");
+                FileInfo file = new FileInfo("C:\\Users\\Ernesto\\Documents\\ar.pdf");
+                byte[] fileLen = BitConverter.GetBytes(file.Length);
+
+                stm.Write(fileLen, 0, fileLen.Length);
+
+                ASCIIEncoding asen = new ASCIIEncoding();
+                byte[] ba = asen.GetBytes(str);
+                Console.WriteLine("Transmitting file name.....");
+
+                stm.Write(ba, 0, ba.Length);
+
+                byte[] bb = new byte[100];
+                int k = stm.Read(bb, 0, 100);
+
+                if (k > 0)
+                {
+                    for (int i = 0; i < k; i++)
+                        Console.Write(Convert.ToChar(bb[i]));
+
+                    Console.WriteLine("Sending file.....");
+                    FileStream fr = File.OpenRead("C:\\Users\\Ernesto\\Documents\\ar.pdf");
+
+                    byte[] fileBytes = new byte[1024];
+                    int byteRead = 0;
+                    while ((byteRead = fr.Read(fileBytes, 0, fileBytes.Length)) > 0)
+                    {
+                        stm.Write(fileBytes, 0, byteRead);
+                    }
+                    fr.Close();
+                    Console.WriteLine("File sent");
+
+                    bb = new byte[100];
+                    k = stm.Read(bb, 0, 100);
+
+                    for (int i = 0; i < k; i++)
+                        Console.Write(Convert.ToChar(bb[i]));
+
+                    
+                }
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine(se.Message);
+                Console.WriteLine("Error..... " + se.StackTrace);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+                Console.WriteLine("Error..... " + exc.StackTrace);
+            }
+            finally
+            {
+                tcpclnt.Close();
+            }
         }
     }
 }
