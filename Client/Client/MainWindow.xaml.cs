d583b1e0188c146e7d4cec381ecdb49a86baff77
ernvalentino88 <ern.valentino@gmail.com>
2016-02-07 22:56:40 +0100

    first window terminated

--- Client/Client/MainWindow.xaml.cs
+++ Client/Client/MainWindow.xaml.cs
@@ -28,9 +28,19 @@ namespace Client
     public partial class MainWindow : MetroWindow
     {
         private delegate void ConnectDelegate(String address, String port);
+        private delegate void RegisterDelegate(String address, String port, String user, String pwd1, String pwd2);
         private delegate void UpdateDelegate(String msg);
         private delegate Task UpdateDelegateAsync(String msg, String bannerTitle, String bannerMsg);
         private Boolean connected;
+        private enum CONNECTION_CODES
+        {
+            ERR,
+            OK,
+            HELLO,
+            AUTH_FAILURE,
+            REG_FAILURE,
+            NEW_REG,
+        };
 
         public MainWindow()
         {
@@ -52,7 +62,7 @@ namespace Client
             if (connected)
             {
                 connected = false;
-                String msg = "Insert IP address and port of the server to connect";
+                String msg = "Log in to the remote server";
                 this.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new UpdateDelegate(updateUI), msg);
             }
             else
@@ -116,7 +126,7 @@ namespace Client
                 catch (SocketException se)
                 {
                     connected = false;
-                    String msg = "Insert IP address and port of the server to connect";
+                    String msg = "Log in to the remote server";
                     StreamWriter sw = new StreamWriter("client_log.txt",true);
                     sw.Write(DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"));
                     sw.WriteLine(" ***Fatal Error***  " + se.Message);
@@ -127,7 +137,7 @@ namespace Client
                 catch (Exception exc)
                 {
                     connected = false;
-                    String msg = "Insert IP address and port of the server to connect";
+                    String msg = "Log in to the remote server";
                     StreamWriter sw = new StreamWriter("client_log.txt",true);
                     sw.Write(DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"));
                     sw.WriteLine(" ***Fatal Error***  " + exc.Message);
@@ -142,18 +152,130 @@ namespace Client
             }
         }
 
+        private void Register_button_Click(object sender, RoutedEventArgs e)
+        {
+            RegisterDelegate rd = new RegisterDelegate(register);
+            rd.BeginInvoke(this.Text_regIp.Text, 
+                this.Text_regPort.Text, 
+                this.Text_regUser.Text, 
+                this.Box_regPwd1.Password, 
+                this.Box_regPwd2.Password, null, null);
+        }
+
+        private void register(String address, String port, String user, String pwd1, String pwd2)
+        {
+            //if (address.Equals(String.Empty) || port.Equals(String.Empty) ||
+            //     user.Equals(String.Empty) || pwd1.Equals(String.Empty) || pwd2.Equals(String.Empty) ||
+            //     address == null || port == null || user == null || pwd1 == null || pwd2 == null)
+            //    return;
+            //if (!(pwd1.Equals(pwd2)))
+            //{
+            //    String msg = "Create account";
+            //    String title = "Registration Failed";
+            //    String msgBanner = "The passwords inserted are different";
+            //    this.Dispatcher.BeginInvoke(DispatcherPriority.Normal, 
+            //        new UpdateDelegateAsync(updateUI_regBanner), msg, title, msgBanner);
+            //}
+            TcpClient tcpclnt = new TcpClient();
+            try
+            {
+                Int32 portInt = Int32.Parse(port);
+                String msg = "Trying to connect to the server . . .";
+                this.Dispatcher.BeginInvoke(DispatcherPriority.Normal, 
+                    new UpdateDelegate(updateUI_progressBarReg), msg);
+                tcpclnt.Connect(address, portInt);
+                
+                Stream stm = tcpclnt.GetStream();
+                String str = "ar.pdf";
+
+                Console.WriteLine("Sending file infos...");
+
+                Console.WriteLine("Transmitting file size.....");
+                FileInfo file = new FileInfo("C:\\Users\\Ernesto\\Documents\\ar.pdf");
+                byte[] fileLen = BitConverter.GetBytes(file.Length);
+
+                stm.Write(fileLen, 0, fileLen.Length);
+
+                ASCIIEncoding asen = new ASCIIEncoding();
+                byte[] ba = asen.GetBytes(str);
+                Console.WriteLine("Transmitting file name.....");
+
+                stm.Write(ba, 0, ba.Length);
+
+                byte[] bb = new byte[100];
+                int k = stm.Read(bb, 0, 100);
+
+                if (k > 0)
+                {
+                    for (int i = 0; i < k; i++)
+                        Console.Write(Convert.ToChar(bb[i]));
+
+                    Console.WriteLine("Sending file.....");
+                    FileStream fr = File.OpenRead("C:\\Users\\Ernesto\\Documents\\ar.pdf");
+
+                    byte[] fileBytes = new byte[1024];
+                    int byteRead = 0;
+                    while ((byteRead = fr.Read(fileBytes, 0, fileBytes.Length)) > 0)
+                    {
+                        stm.Write(fileBytes, 0, byteRead);
+                    }
+                    fr.Close();
+                    Console.WriteLine("File sent");
+
+                    bb = new byte[100];
+                    k = stm.Read(bb, 0, 100);
+
+                    for (int i = 0; i < k; i++)
+                        Console.Write(Convert.ToChar(bb[i]));
+
+
+                }
+            }
+            catch (SocketException se)
+            {
+                String msg = "Create Account";
+                StreamWriter sw = new StreamWriter("client_log.txt", true);
+                sw.Write(DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"));
+                sw.WriteLine(" ***Fatal Error***  " + se.Message);
+                sw.WriteLine(se.StackTrace);
+                sw.Close();
+                this.Dispatcher.BeginInvoke(DispatcherPriority.Normal, 
+                    new UpdateDelegateAsync(updateUI_regBanner), msg, 
+                    "Server Unreachable", se.Message);
+            }
+            catch (Exception exc)
+            {
+                String msg = "Create Account";
+                StreamWriter sw = new StreamWriter("client_log.txt", true);
+                sw.Write(DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"));
+                sw.WriteLine(" ***Fatal Error***  " + exc.Message);
+                sw.WriteLine(exc.StackTrace);
+                sw.Close();
+                //this.Dispatcher.BeginInvoke(DispatcherPriority.Normal, 
+                //    new UpdateDelegate(updateUI), msg);
+            }
+            finally
+            {
+                tcpclnt.Close();
+            }
+        }
+
         public void updateUI(String msg)
         {
             if (connected)
             {
                 this.Text_ip.Visibility = Visibility.Hidden;
                 this.Text_port.Visibility = Visibility.Hidden;
+                this.Text_user.Visibility = Visibility.Hidden;
+                this.Box_pwd.Visibility = Visibility.Hidden;
                 this.Connect_button.Content = "disconnect";
             }
             else
             {
                 this.Text_ip.Visibility = Visibility.Visible;
                 this.Text_port.Visibility = Visibility.Visible;
+                this.Text_user.Visibility = Visibility.Visible;
+                this.Box_pwd.Visibility = Visibility.Visible;
                 this.Connect_button.Content = "connect";
             }
             this.progress_bar.Visibility = Visibility.Hidden;
@@ -165,18 +287,47 @@ namespace Client
             await this.ShowMessageAsync(bannerTitle, bannerMsg);
             this.Text_ip.Visibility = Visibility.Visible;
             this.Text_port.Visibility = Visibility.Visible;
+            this.Text_user.Visibility = Visibility.Visible;
+            this.Box_pwd.Visibility = Visibility.Visible;
             this.Connect_button.Content = "connect";
             this.progress_bar.Visibility = Visibility.Hidden;
             this.Message_label.Content = msg;
+        }
+
+        public async Task updateUI_regBanner(String msg, String bannerTitle, String bannerMsg)
+        {
+            await this.ShowMessageAsync(bannerTitle, bannerMsg);
+            this.Text_regIp.Visibility = Visibility.Visible;
+            this.Text_regPort.Visibility = Visibility.Visible;
+            this.Text_regUser.Visibility = Visibility.Visible;
+            this.Box_regPwd1.Visibility = Visibility.Visible;
+            this.Box_regPwd2.Visibility = Visibility.Visible;
+            this.Register_button.Visibility = Visibility.Visible;
+            this.progressBar_reg.Visibility = Visibility.Hidden;
+            this.label_regMsg.Content = msg;
         }
 
         public void updateUI_progressBar(String msg)
         {
             this.Text_ip.Visibility = Visibility.Hidden;
             this.Text_port.Visibility = Visibility.Hidden;
+            this.Text_user.Visibility = Visibility.Hidden;
+            this.Box_pwd.Visibility = Visibility.Hidden;
             this.Connect_button.Content = "disconnect";
             this.progress_bar.Visibility = Visibility.Visible;
             this.Message_label.Content = msg;
+        }
+
+        public void updateUI_progressBarReg(String msg)
+        {
+            this.Text_regIp.Visibility = Visibility.Hidden;
+            this.Text_regPort.Visibility = Visibility.Hidden;
+            this.Text_regUser.Visibility = Visibility.Hidden;
+            this.Box_regPwd1.Visibility = Visibility.Hidden;
+            this.Box_regPwd2.Visibility = Visibility.Hidden;
+            this.Register_button.Visibility = Visibility.Hidden;
+            this.progressBar_reg.Visibility = Visibility.Visible;
+            this.label_regMsg.Content = msg;
         }
 
     }
